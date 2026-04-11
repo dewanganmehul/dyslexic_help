8d388439c8114e3796d833533c48ff1d71f451e0
dewanganmehul <mehuldewangan004@gmail.com>
2026-04-11 16:36:53 +0530

    fixed env

--- server/index.js
+++ server/index.js
@@ -23,4 +23,8 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
